e2c66134c48a1268344615b9994bfe0e58e64b92
amzotti <azotti1337@gmail.com>
2014-12-23 02:16:09 -0800

    Implement getElementsByClassName
    Use recursion to traverse entire DOM tree
    
    Lessons Learned:
    The DOM is an object oriented representation of the current document.
    
    The DOM is NOT JavaScript specific, but rather it could work with any
    programming language because it is a language-independent API.
    
    We can access the DOM tree with:
    document.body
    
    object.className provides the class name of the object in question

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+
+var getElementsByClassName = function(klass) {
+  var className = klass;
+  var classElements = [];
+  var getElementsByClassNameHelper = function(root){
+
+    var classNames = root.className.split(" ");
+    for (var i = 0;i < classNames.length;i++) {
+      if (classNames[i] === className)
+        classElements.push(root);
+    }
+
+    var children = root.children;
+    if (children !== undefined) {
+      for (i = 0;i < children.length;i++)
+        getElementsByClassNameHelper(children[i]);
+    }
+  };
+  getElementsByClassNameHelper(document.body);
+  return classElements;
 };
